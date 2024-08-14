2f9349ab291025801023b8be0b6f1342d94651d9
Felipe Gerardo Viana Figueroa <gerardo_viana@hotmail.com>
2024-08-13 19:59:58 -0600

    Creacion de index.php con estructura html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+   <head> 
+      <title> Felipe Gerardo Viana Figueroa</title>
+      <link rel="stylesheet" href="css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="css/style.css"/>
+      <script src ="js/jquery-3.7.1.min.js"></script>
+      <script src="js/bootsrap.bundle.min.js"></script>
+   </head> 
+   <body>
+
+   </body>
+</html>
